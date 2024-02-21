99d974e9bbeffa9ae35f85f4c4db8944d08edc8f
Nikolai Telin <telinnikolai@gmail.com>
2024-02-21 13:03:18 +0000

    conf

--- config.py
+++ config.py
@@ -10,16 +10,16 @@ import os
 load_dotenv(override=True)
 
 TOKEN = os.environ.get("TOKEN")
-#database = os.environ.get("DATABASE")
-#host = os.environ.get("HOST")
-#user = os.environ.get("USER")
-#password = os.environ.get("PASSWORD")
-#port = os.environ.get("PORT")
+database = os.environ.get("DATABASE")
+host = os.environ.get("HOST")
+user = os.environ.get("USER")
+password = os.environ.get("PASSWORD")
+port = os.environ.get("PORT")
 
 ###############################################################################
-database='postgres'
-host='psychobot.postgres.database.azure.com'
-user='alexmodestov'
-password='PostgreSQL!#'
-port=5432
-DATABASE_URL = 'postgresql+asyncpg://alexmodestov:PostgreSQL!#@psychobot.postgres.database.azure.com:5432/postgres'
+#database='postgres'
+#host='psychobot.postgres.database.azure.com'
+#user='alexmodestov'
+#password='PostgreSQL!#'
+#port=5432
+#DATABASE_URL = 'postgresql+asyncpg://alexmodestov:PostgreSQL!#@psychobot.postgres.database.azure.com:5432/postgres'
